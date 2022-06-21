94c684ec15a1dd3bedab96b7c3d3c742637ef5d4
Asher233 <75360017+Asher233@users.noreply.github.com>
2022-06-21 11:27:11 +0800

    Update sub.js

--- Script/sub.js
+++ Script/sub.js
@@ -1,7 +1,7 @@
 /***********************************
 
 # > 通用订阅解锁 hostname = buy.itunes.apple.com
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body 
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Asher233/DIY/main/Script/sub.js
 
 ***********************************/
 
