a6f72b49a2d90d4cb7ebe37074234bfa24175e72
Asher233 <75360017+Asher233@users.noreply.github.com>
2022-07-14 18:06:56 +0800

    Update subs.js

--- Script/subs.js
+++ Script/subs.js
@@ -1,6 +1,6 @@
 /***********************************
 
-# > subs = hostname = api.revenuecat.com
+> # 通用订阅解锁 = hostname = api.revenuecat.com
 ^https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/Asher233/DIY/main/Script/subs.js
 
 ***********************************/
